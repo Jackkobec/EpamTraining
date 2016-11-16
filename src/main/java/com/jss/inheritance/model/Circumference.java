904f4b37bd202a642922d4a6eb7bbcb23fc2b185
jackkobec <jackkobec@gmail.com>
2016-11-16 18:24:49 +0200

    Inheritance some fixes.

--- src/main/java/com/jss/inheritance/model/Circumference.java
+++ src/main/java/com/jss/inheritance/model/Circumference.java
@@ -16,31 +16,4 @@ public abstract class Circumference {
         this.center = center;
         this.radius = radius;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Circumference that = (Circumference) o;
-
-        if (radius != that.radius) return false;
-        return center != null ? center.equals(that.center) : that.center == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = center != null ? center.hashCode() : 0;
-        result = 31 * result + radius;
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Circumference{" +
-                "center=" + center +
-                ", radius=" + radius +
-                '}';
-    }
 }
