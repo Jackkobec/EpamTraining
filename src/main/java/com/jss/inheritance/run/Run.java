be3552782fad5e4611c9718964782aa137f784ae
jackkobec <jackkobec@gmail.com>
2016-11-16 18:41:08 +0200

    Inheritance some fixes.

--- src/main/java/com/jss/inheritance/run/Run.java
+++ src/main/java/com/jss/inheritance/run/Run.java
@@ -19,6 +19,11 @@ public final class Run {
         Graphics app = new Graphics();
         ControlPanel controlPanel = new ControlPanel(app);
 
+        testEqualsAndHashcode();
+    }
+
+    public static void testEqualsAndHashcode() {
+
         Circle circle1 = new Factory().createCircle(2, 2, 3);
         Circle circle12 = new Factory().createCircle(2, 2, 3);
 
@@ -27,6 +32,14 @@ public final class Run {
         System.out.println("circle12 hashcode: " + circle12.hashCode());
         System.out.println("Сравнение hashcodes "
                 + (circle1.hashCode() == circle12.hashCode() ? "true" : "false"));
+        System.out.println("=========================");
 
+        Circle circle13 = new Factory().createCircle(3, 3, 4);
+
+        System.out.println("circle1 equals circle13: " + circle1.equals(circle13));
+        System.out.println("circle1 hashcode: " + circle1.hashCode());
+        System.out.println("circle13 hashcode: " + circle13.hashCode());
+        System.out.println("Сравнение hashcodes "
+                + (circle1.hashCode() == circle13.hashCode() ? "true" : "false"));
     }
 }
